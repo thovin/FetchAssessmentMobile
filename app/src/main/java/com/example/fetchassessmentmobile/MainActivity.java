bea5c7d425039f752d145bc3522f80f0d8206bd5
TJordan <Tjordan7@stu.jsu.edu>
2023-02-09 17:01:17 -0600

    Clean up visual design

--- app/src/main/java/com/example/fetchassessmentmobile/MainActivity.java
+++ app/src/main/java/com/example/fetchassessmentmobile/MainActivity.java
@@ -23,7 +23,6 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.Comparator;
 import java.util.HashMap;
 
@@ -66,7 +65,7 @@ public class MainActivity extends AppCompatActivity {
             }
 
             outputStrings.add(sb.toString());
-            outputStrings.set(0, new StringBuilder(outputStrings.get(0)).append("\nlistId: ").append(listIds[i])
+            outputStrings.set(0, new StringBuilder(outputStrings.get(0)).append("\n\nlistId: ").append(listIds[i])
                     .append("\n").append(sb.toString()).toString());
         }
 
